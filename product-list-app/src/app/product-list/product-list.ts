eafddbb3a94f7f329e9c33082826b98a50d32526
Mohamed Hatem <mohatem149@gmail.com>
2025-07-26 19:06:41 +0300

    Add files via upload

---
+++ product-list-app/src/app/product-list/product-list.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-list.html',
+  styleUrl: './product-list.css'
+})
+export class ProductListComponent implements OnInit {
+  products: any[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+    });
+  }
+} 
